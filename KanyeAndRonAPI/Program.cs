d2f2faa41cd467fdac979b9c303464385c4c7d94
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-10-12 08:19:31 -0500

    set up a get request to the kanye api

--- KanyeAndRonAPI/Program.cs
+++ KanyeAndRonAPI/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+// Making a New Client Object //
+// Provies a base class for sending HTTP Requests and receiving HTTP Responses //
+var client = new HttpClient();
+
+// String that holds the URL //
+var kanyeURL = "https://api.kanye.rest/";
+// Put inside of the Get String Async Method // 
+var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+// Now we need to Parse the Response //
+// Need to add a nuget package Newtonsoft. Allows us to work with JSON //
+var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+Console.WriteLine($"Kanye: {kanyeQuote}");
+Console.ReadKey();
 
